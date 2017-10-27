f63b11bec8843ef92f26bb1566cf5c97f329a724
cjperes <caio@bbcreative.org>
2017-10-27 15:32:19 -0200

    novas funções boladas

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
-﻿add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+﻿<?php
+
+#remover versao do wordpress
+function wpb_remove_version() {
+return '';
+}
+add_filter('the_generator', 'wpb_remove_version'); 
+
+
+
+
+#custom painel no feed
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets'); 
   
 function my_custom_dashboard_widgets() {
 global $wp_meta_boxes;
@@ -7,6 +19,40 @@ wp_add_dashboard_widget('custom_help_widget', 'Nork Digital', 'custom_dashboard_
 }
  
 function custom_dashboard_help() {
-echo '<p>Bem-vindo! Precisa de ajuda? <a href="mailto:hello@nork.digital">entre em contato</a>. Veja alguns tutoriais que podem te ajudar: <a href="https://www.nork.digital/help/wordpress" target="_blank">Help Nork</a></p>';
+echo '<p>Bem-vindo ao seu site Fast Nork! Precisa de ajuda? <a href="mailto:hello@nork.digital">entre em contato</a>. Veja alguns tutoriais que podem te ajudar: <a href="https://www.nork.digital/help/wordpress" target="_blank">Help Nork</a></p>';
 echo '<img src="https://www.nork.digital/wp-content/uploads/2017/10/nork-wordpress.png" />';
 }
+
+
+
+#remover footer
+function remove_footer_admin () {
+
+echo 'Desenvolvido por <a href="https://www.nork.digital" target="_blank">Nork Digital</a> | Precisa de ajuda? <a href="mailto:suporte@nork.digital" target="_blank">Suporte</a></p>';
+
+}
+
+
+add_filter('admin_footer_text', 'remove_footer_admin');
+
+
+
+
+
+#remover feedback de erro wordpress
+function no_wordpress_errors(){
+  return 'Algo deu errado, tente novamente.';
+}
+add_filter( 'login_errors', 'no_wordpress_errors' );
+
+
+
+
+#remover painel de boas vindas
+remove_action('welcome_panel', 'wp_welcome_panel');
+
+
+
+
+
+?>

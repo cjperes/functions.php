6f03a189fe111d98b4a423dd777468627247aa8c
cjperes <caio@bbcreative.org>
2017-10-27 12:29:34 -0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,12 @@
+﻿add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+  
+function my_custom_dashboard_widgets() {
+global $wp_meta_boxes;
+ 
+wp_add_dashboard_widget('custom_help_widget', 'Nork Digital', 'custom_dashboard_help');
+}
+ 
+function custom_dashboard_help() {
+echo '<p>Bem-vindo! Precisa de ajuda? <a href="mailto:hello@nork.digital">entre em contato</a>. Veja alguns tutoriais que podem te ajudar: <a href="https://www.nork.digital/help/wordpress" target="_blank">Help Nork</a></p>';
+echo '<img src="https://www.nork.digital/wp-content/uploads/2017/10/nork-wordpress.png" />';
+}
